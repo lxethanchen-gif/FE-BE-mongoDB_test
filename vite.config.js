0b54be41bbc75a7a06eba4879ab81f884583e305
unknown <lxethanchen@gmail.com>
2026-06-23 02:53:26 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   // 加上這行，讓打包後的資源能正確對應 GitHub Pages 的路徑
   // 注意：前後都要有斜線，例如 '/my-website/'
-  base: '/你的儲存庫名稱/',
+  base: 'FE-BE-mongoDB_test',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
